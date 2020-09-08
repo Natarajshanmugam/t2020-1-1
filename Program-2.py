34f1af7aa806df9feef61d3f7ccf03fdd1bbcb76
Nataraj <50852569+Natarajshanmugam@users.noreply.github.com>
2020-09-08 18:29:08 +0530

    Add files via upload

---
+++ Program-2.py
@@ -0,0 +1,11 @@
+a = int(input())
+output_list = []
+odd_number = 1
+
+##Run the loop until length ouput_list is equal to a
+while(len(output_list)<a):
+    output_list.append(odd_number)
+    odd_number+=2
+print(output_list)
+    
+    
